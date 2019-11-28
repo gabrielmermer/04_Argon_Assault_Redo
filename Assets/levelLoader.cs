9a90933cf45aeed7e4eeeba0a2b6a0bbe76610fb
zubrzysta <zubrzysta@gmail.com>
2019-11-28 09:41:50 +0100

    timeline prototype

---
+++ Assets/levelLoader.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class levelLoader : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    void Start()
+    {
+        Invoke("LoadLevel", 2f);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LoadLevel()
+    {
+        SceneManager.LoadScene("Level 1");
+    }
+}
