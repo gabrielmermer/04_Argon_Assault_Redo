346952ae402dba8aa418ad96bd9b9a665a285a1d
zubrzysta <zubrzysta@gmail.com>
2019-12-12 09:23:38 +0100

    86

---
+++ Assets/player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class player : MonoBehaviour
+{
+    [Tooltip("In ms^-1")][SerializeField] float xSpeed = 4f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+        print(xOffset);
+        transform.localPosition = new Vector3(xOffset, transform.localPosition.y, transform.localPosition.z);
+
+        
+    }
+}
